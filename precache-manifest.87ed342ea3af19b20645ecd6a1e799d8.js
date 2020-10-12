28914bed3d102323da2d0e482fe3afb1bcc606bf
Artem Kvitka <kvitkaart554@gmail.com>
2020-10-12 18:23:40 +0300

    Updates

---
+++ precache-manifest.87ed342ea3af19b20645ecd6a1e799d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eb3b7d67f7f4f881ab7ee8485921c32",
+    "url": "/facebook-clone/index.html"
+  },
+  {
+    "revision": "16c63226a490253bf4ce",
+    "url": "/facebook-clone/static/js/2.ff263b6b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/facebook-clone/static/js/2.ff263b6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "489abfd95fa485510d6e",
+    "url": "/facebook-clone/static/js/main.2f163845.chunk.js"
+  },
+  {
+    "revision": "8d9db31eeef904c1dc55",
+    "url": "/facebook-clone/static/js/runtime-main.389e76a3.js"
+  },
+  {
+    "revision": "c9247556d69a94474d410e6851e054a3",
+    "url": "/facebook-clone/static/media/profile.c9247556.svg"
+  }
+]);
